1fa84f97ddc33cdf96c49d7f157e4d2d620963ef
Quantum Explorer <quantum@dash.org>
2018-03-04 09:19:01 +0700

    Initial Commit

---
+++ dashsync/dashsync.h
@@ -0,0 +1,19 @@
+//
+//  dashsync.h
+//  dashsync
+//
+//  Created by Sam Westrich on 3/4/18.
+//  Copyright © 2018 dashcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dashsync.
+FOUNDATION_EXPORT double dashsyncVersionNumber;
+
+//! Project version string for dashsync.
+FOUNDATION_EXPORT const unsigned char dashsyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dashsync/PublicHeader.h>
+
+
