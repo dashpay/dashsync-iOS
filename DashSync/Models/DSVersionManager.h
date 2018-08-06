59d9c275c49fe917e711281e8c868cf891aba6c7
Andrew Podkovyrin <podkovyrin@gmail.com>
2018-08-06 12:03:30 +0300

    Fix DSVersionManager interface

--- DashSync/Models/DSVersionManager.h
+++ DashSync/Models/DSVersionManager.h
@@ -7,7 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-@class DSChain;
+@class DSWallet;
 
 typedef void (^UpgradeCompletionBlock)(BOOL success, BOOL neededUpgrade,BOOL authenticated,BOOL cancelled); //success is true is neededUpgrade is true and we upgraded, or we didn't need upgrade
 
@@ -15,7 +15,9 @@ typedef void (^UpgradeCompletionBlock)(BOOL success, BOOL neededUpgrade,BOOL aut
 
 + (instancetype _Nullable)sharedInstance;
 
--(void)upgradeExtendedKeysWithCompletion:(_Nullable UpgradeCompletionBlock)completion forChain:(DSChain*)chain;
+- (BOOL)hasAOldWallet;
+
+-(void)upgradeExtendedKeysForWallet:(DSWallet*)wallet withCompletion:(UpgradeCompletionBlock)completion;
 
 - (void)clearKeychainWalletData;
 
