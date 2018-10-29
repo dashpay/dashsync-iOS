6f9c5c6a4bc1f974ec69099e425df374f7c35947
Quantum Explorer <quantum@dash.org>
2018-10-29 17:54:42 +0700

    fixed sync from genesis

--- DashSync/Models/DSOptionsManager.m
+++ DashSync/Models/DSOptionsManager.m
@@ -64,7 +64,7 @@
 - (BOOL)syncFromGenesis {
     NSString *key = @"syncFromHeight";
     id syncFromHeight = [[self userDefaults] objectForKey:key];
-    if (syncFromHeight) {
+    if ([syncFromHeight integerValue]) {
         return !self.syncFromHeight;
     }
     else {
