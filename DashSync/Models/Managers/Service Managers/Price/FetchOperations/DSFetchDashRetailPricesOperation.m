c15083917634a2dbd24fcc84b4d931f0a6751dfa
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-05-21 18:24:09 +0300

    Restore DashRetail price sourcing operation
    (cherry picked from commit e7c3dc0ab662350c4a4cc1b48fe89853f20d0610)

---
+++ DashSync/Models/Managers/Service Managers/Price/FetchOperations/DSFetchDashRetailPricesOperation.m
@@ -0,0 +1,65 @@
+//
+//  Created by Andrew Podkovyrin
+//  Copyright © 2019 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "DSFetchDashRetailPricesOperation.h"
+
+#import "DSHTTPDashRetailOperation.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define DASHRETAIL_TICKER_URL @"https://rates2.dashretail.org/rates?source=dashretail"
+
+@interface DSFetchDashRetailPricesOperation ()
+
+@property (strong, nonatomic) DSHTTPDashRetailOperation *dashRetailOperation;
+
+@property (copy, nonatomic) void (^fetchCompletion)(NSArray<DSCurrencyPriceObject *> *_Nullable);
+
+@end
+
+@implementation DSFetchDashRetailPricesOperation
+
+- (DSOperation *)initOperationWithCompletion:(void (^)(NSArray<DSCurrencyPriceObject *> *_Nullable))completion {
+    self = [super initWithOperations:nil];
+    if (self) {
+        HTTPRequest *request = [HTTPRequest requestWithURL:[NSURL URLWithString:DASHRETAIL_TICKER_URL]
+                                                    method:HTTPRequestMethod_GET
+                                                parameters:nil];
+        request.timeout = 30.0;
+        request.cachePolicy = NSURLRequestReloadIgnoringCacheData;
+
+        DSHTTPDashRetailOperation *operation = [[DSHTTPDashRetailOperation alloc] initWithRequest:request];
+        _dashRetailOperation = operation;
+        _fetchCompletion = [completion copy];
+
+        [self addOperation:operation];
+    }
+    return self;
+}
+
+- (void)finishedWithErrors:(NSArray<NSError *> *)errors {
+    if (self.cancelled) {
+        return;
+    }
+
+    NSArray<DSCurrencyPriceObject *> *prices = self.dashRetailOperation.prices;
+    self.fetchCompletion(prices);
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DashSync/Models/Managers/Service Managers/Price/Requests/DSHTTPDashRetailOperation.m
@@ -0,0 +1,79 @@
+//
+//  Created by Andrew Podkovyrin
+//  Copyright © 2019 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "DSHTTPDashRetailOperation.h"
+
+#import "DSCurrencyPriceObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DSHTTPDashRetailOperation ()
+
+@property (copy, nonatomic, nullable) NSArray<DSCurrencyPriceObject *> *prices;
+
+@end
+
+@implementation DSHTTPDashRetailOperation
+
+- (void)processSuccessResponse:(id)parsedData responseHeaders:(NSDictionary *)responseHeaders statusCode:(NSInteger)statusCode {
+    NSParameterAssert(parsedData);
+
+    NSArray *response = (NSArray *)parsedData;
+    if (![response isKindOfClass:NSArray.class]) {
+        [self cancelWithInvalidResponse:response];
+
+        return;
+    }
+
+    BOOL responseIsValid = YES;
+    for (id value in response) {
+        responseIsValid = [value isKindOfClass:NSDictionary.class] &&
+            [value[@"price"] isKindOfClass:NSString.class] &&
+            [value[@"symbol"] isKindOfClass:NSString.class];
+        if (!responseIsValid) {
+            break;
+        }
+    }
+
+    if (!responseIsValid) {
+        [self cancelWithInvalidResponse:response];
+
+        return;
+    }
+
+    NSMutableArray<DSCurrencyPriceObject *> *prices = [NSMutableArray array];
+    for (NSDictionary *rawPriceObject in response) {
+        NSString *symbol = rawPriceObject[@"symbol"];
+        if (![symbol hasPrefix:@"DASH"]) {
+            continue;
+        }
+        NSString *code = [symbol substringFromIndex:4];
+        NSNumber *price = @([rawPriceObject[@"price"] doubleValue]);
+        DSCurrencyPriceObject *priceObject = [[DSCurrencyPriceObject alloc] initWithCode:code price:price];
+        if (priceObject) {
+            [prices addObject:priceObject];
+        }
+    }
+
+    self.prices = prices;
+
+    [self finish];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
