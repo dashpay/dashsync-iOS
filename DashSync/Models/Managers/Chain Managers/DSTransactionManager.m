fdc8c19942fe06254c2d2e5d07dbfd5017cb6433
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-09-06 13:50:45 +0300

    Improve DSTransactionManager API

--- DashSync/Models/Managers/Chain Managers/DSTransactionManager.m
+++ DashSync/Models/Managers/Chain Managers/DSTransactionManager.m
@@ -447,6 +447,8 @@
         address = [address stringByAppendingFormat:@"%@%@", (address.length > 0) ? @", " : @"", addr];
     }
     
+    NSParameterAssert(address);
+    
     NSString *name = protoReq.commonName;
     NSString *memo = protoReq.details.memo;
     BOOL isSecure = (valid && ! [protoReq.pkiType isEqual:@"none"]);
@@ -459,7 +461,7 @@
                                                                                  isSecure:isSecure
                                                                              errorMessage:@""
                                                                             localCurrency:localCurrency];
-    if (transactionCreationCompletion(tx,suggestedPrompt,amount, address, isSecure)) {
+    if (transactionCreationCompletion(tx,suggestedPrompt,amount, @[address], isSecure)) {
         CFRunLoopPerformBlock([[NSRunLoop mainRunLoop] getCFRunLoop], kCFRunLoopCommonModes, ^{
             [self signAndPublishTransaction:tx createdFromProtocolRequest:protoReq fromAccount:account toAddress:address withPrompt:suggestedPrompt forAmount:amount requestingAdditionalInfo:additionalInfoRequest presentChallenge:challenge transactionCreationCompletion:transactionCreationCompletion signedCompletion:signedCompletion publishedCompletion:publishedCompletion requestRelayCompletion:requestRelayCompletion errorNotificationBlock:errorNotificationBlock];
         });
