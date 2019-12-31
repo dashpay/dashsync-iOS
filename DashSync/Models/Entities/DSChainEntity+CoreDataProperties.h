06568476d6948a3686c83aadd5d5d4accdb66093
Quantum Explorer <quantum@dash.org>
2019-12-31 13:16:51 -0800

    updated DSChainEntity

--- DashSync/Models/Entities/DSChainEntity+CoreDataProperties.h
+++ DashSync/Models/Entities/DSChainEntity+CoreDataProperties.h
@@ -18,11 +18,11 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, retain) NSData *baseBlockHash;
 @property (nullable, nonatomic, retain) NSData *checkpoints;
 @property (nullable, nonatomic, copy) NSString *devnetIdentifier;
-@property (nullable, nonatomic, copy) NSNumber *standardDapiPort;
-@property (nullable, nonatomic, copy) NSNumber *standardPort;
-@property (nullable, nonatomic, copy) NSNumber *totalGovernanceObjectsCount;
-@property (nullable, nonatomic, copy) NSNumber *totalMasternodeCount;
-@property (nullable, nonatomic, copy) NSNumber *type;
+@property (nonatomic, assign) uint32_t standardDapiPort;
+@property (nonatomic, assign) uint32_t standardPort;
+@property (nonatomic, assign) uint32_t totalGovernanceObjectsCount;
+@property (nonatomic, assign) uint32_t totalMasternodeCount;
+@property (nonatomic, assign) uint16_t type;
 @property (nullable, nonatomic, retain) NSSet<DSAccountEntity *> *accounts;
 @property (nullable, nonatomic, retain) NSSet<DSMerkleBlockEntity *> *blocks;
 @property (nullable, nonatomic, retain) NSSet<DSContactEntity *> *contacts;
