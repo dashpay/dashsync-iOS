088de1c0a7b674da78532905db1a402118cf56c1
Quantum Explorer <quantum@dash.org>
2021-01-13 07:56:58 +0700

    linting and CI

--- Example/run_oclint.sh
+++ Example/run_oclint.sh
@@ -3,18 +3,6 @@
 # Create input for OCLint
 xcodebuild clean build -workspace DashSync.xcworkspace -scheme DashSync-Example -sdk iphonesimulator COMPILER_INDEX_STORE_ENABLE=NO ONLY_ACTIVE_ARCH=NO | xcpretty -r json-compilation-database --output compile_commands.json
 
-
-# Rules
-LINT_RULES="-rc LONG_LINE=300 \
-    -rc LONG_VARIABLE_NAME=100 \
-    -rc LONG_METHOD=200 \
-    -rc SHORT_VARIABLE_NAME=0"
-
-# Threshold
-LINT_THRESHOLD="-max-priority-1=0 \
-    -max-priority-2=200 \
-    -max-priority-3=300"
-
 # Excludes
 # grep-like syntax
 LINT_EXCLUDES="Pods|Example"
