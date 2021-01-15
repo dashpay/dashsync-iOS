02fe995cfd24d0559d02bd43ad23b9397d0047eb
Quantum Explorer <quantum@dash.org>
2021-01-15 13:56:31 +0700

    using test plans

--- Example/Tests/DSChainedSigningTests.m
+++ Example/Tests/DSChainedSigningTests.m
@@ -114,11 +114,4 @@
     //at node n, quorum checks that signature matches
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
