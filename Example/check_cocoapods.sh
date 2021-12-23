34ab0646e3a5384a55d12a5f4d89a9387faadca1
Pavel Tikhonenko <tikhop@gmail.com>
2021-12-23 12:55:49 +0300

    Fixing CI

--- Example/check_cocoapods.sh
+++ Example/check_cocoapods.sh
@@ -1,4 +1,4 @@
-CUR_COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ./Podfile.lock`
+CUR_COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 ENV_COCOAPODS_VER=`pod --version`
 
 # check if not the same version, reinstall cocoapods version to current project's
