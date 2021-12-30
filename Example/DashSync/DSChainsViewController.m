5eb9081c8c09571cba818ec3429ccb11975ee788
pankcuf <rastacake@gmail.com>
2021-12-30 23:45:22 +0300

    [fix: reload ui after add chain]

--- Example/DashSync/DSChainsViewController.m
+++ Example/DashSync/DSChainsViewController.m
@@ -34,6 +34,11 @@
                                                       }];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
